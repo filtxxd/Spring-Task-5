5177b99aa538e28a0c75abcbc5f05ca4331eedca
filtxxd <filtxxd@gmail.com>
2024-08-18 10:00:31 +0300

    new commit

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,75 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars2")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String model;
+
+    @Column
+    private int series;
+
+    @OneToOne(mappedBy ="car", cascade = CascadeType.ALL)
+    private User user;
+
+    public Car () {
+
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+
+    public Car setUser(User user) {
+        this.user = user;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                ", user=" + user +
+                '}';
+    }
+
+}
